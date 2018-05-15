a21ce930b84453c211e0198e9a07b9c14169a559
Vinicius Severo <vinicius.severo@outlook.com>
2018-05-14 22:02:37 -0300

    teste

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var app = require('./config/server');
 var server_port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || process.env.GERENCIADOR_ALUNOS_SERVICE_HOST || '0.0.0.0';
+var server_ip_address = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
  
 app.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ", port " + server_port )
